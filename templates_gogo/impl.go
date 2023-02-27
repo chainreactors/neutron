3cc29439713e4081bfa32094760d75125918252e
M09Ic <m09ician@gmail.com>
2023-02-27 20:18:50 +0800

    添加gogo专属templates

---
+++ templates_gogo/impl.go
@@ -0,0 +1,49 @@
+package templates
+
+import (
+	"github.com/chainreactors/neutron/operators"
+	"github.com/chainreactors/neutron/protocols"
+	"github.com/chainreactors/neutron/protocols/executer"
+	"strings"
+)
+
+func (t *Template) GetTags() []string {
+	if t.Info.Tags != "" {
+		return strings.Split(t.Info.Tags, ",")
+	}
+	return []string{}
+}
+
+func (t *Template) Compile(options *protocols.ExecuterOptions) error {
+	var requests []protocols.Request
+	var err error
+	if len(t.RequestsHTTP) > 0 {
+		for _, req := range t.RequestsHTTP {
+			requests = append(requests, &req)
+		}
+		t.Executor = executer.NewExecuter(requests, options)
+	}
+	if len(t.RequestsNetwork) > 0 {
+		for _, req := range t.RequestsNetwork {
+			requests = append(requests, &req)
+		}
+		t.Executor = executer.NewExecuter(requests, options)
+	}
+
+	if t.Executor != nil {
+		err = t.Executor.Compile()
+		if err != nil {
+			return err
+		}
+		t.TotalRequests += t.Executor.Requests()
+	}
+	return nil
+}
+
+func (t *Template) Execute(input string) (*operators.Result, bool) {
+	res, err := t.Executor.Execute(input)
+	if err != nil || res == nil {
+		return nil, false
+	}
+	return res, true
+}
