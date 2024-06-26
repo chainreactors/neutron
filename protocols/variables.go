d296961147d67334a35bce1be22e7276354ca628
M09Ic <m09ician@gmail.com>
2024-06-26 18:12:48 +0800

    git add variables.go

---
+++ protocols/variables.go
@@ -0,0 +1,27 @@
+package protocols
+
+import (
+	"github.com/chainreactors/neutron/common"
+)
+
+type Variable map[string]interface{}
+
+// Evaluate returns a finished map of variables based on set values
+func (variables *Variable) Evaluate(values map[string]interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	for key, value := range *variables {
+		result[key] = evaluateVariableValue(common.ToString(value), values, result)
+	}
+	return result
+}
+
+// evaluateVariableValue expression and returns final value
+func evaluateVariableValue(expression string, values, processing map[string]interface{}) string {
+	finalMap := common.MergeMaps(values, processing)
+
+	result, err := common.Evaluate(expression, finalMap)
+	if err != nil {
+		return expression
+	}
+	return result
+}
